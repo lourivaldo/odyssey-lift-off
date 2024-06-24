1f2327939cedab02fdba4c3b0850dbffe6e98c51
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2024-06-24 19:15:19 -0300

    feat: mocked query data

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import {mocks} from "./mocks";
+
+async function startApolloServer() {
+    const server = new ApolloServer({
+        schema: addMocksToSchema({
+            schema: makeExecutableSchema({ typeDefs }),
+            mocks
+        }),
+    });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
